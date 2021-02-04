d9758ecab680fea90468b4163412427890ae267a
rocleemusic <48lee@cardinalmail.cua.edu>
2021-02-03 23:09:28 -0500

    Add experience script to prefabs

--- Assets/Scripts/Experience.cs
+++ Assets/Scripts/Experience.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Experience : MonoBehaviour
 {
     [Header("Unit Properties")]
-    [SerializeField] int unitLevel;
+    [SerializeField] int unitLevel = 1;
     [SerializeField] float experience;
 
     [Header("Gives Exp if Killed")]

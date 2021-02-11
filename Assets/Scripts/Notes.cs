f3c05ec2612cb6a10e1db04e09b888b6310162fa
rocleemusic <48lee@cardinalmail.cua.edu>
2021-02-11 11:37:52 -0500

    create prefab for dwarf infantry

---
+++ Assets/Scripts/Notes.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    [TextArea(3, 10)]
+    public string notes = "A place to write notes!";
+}

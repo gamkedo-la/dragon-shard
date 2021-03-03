ae9f920a28d04a7c63c22bd1b83603b79f957c23
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2021-03-03 15:56:57 -0800

    placeholder end of level dialog

--- Assets/Scripts/Dialog/HPLevelDialogTrigger.cs
+++ Assets/Scripts/Dialog/HPLevelDialogTrigger.cs
@@ -7,13 +7,12 @@ public class HPLevelDialogTrigger : MonoBehaviour
     public DialogEvent thisDialog;
     public int HPTriggerLevel;
 
-    public bool DialogStarted;
 
     HitPoints hit;
     // Start is called before the first frame update
     void Start()
     {
-        hit = GetComponent<HitPoints>();
+
     }
 
     // Update is called once per frame

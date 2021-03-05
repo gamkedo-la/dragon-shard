7563b43fbe7c66b210fe1338906e7740048167e4
rocleemusic <48lee@cardinalmail.cua.edu>
2021-03-05 18:27:38 -0500

    Audio implementation assets

---
+++ Assets/Scripts/Audio/AudioEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+[CreateAssetMenu(menuName = "Dragon Shard/AudioEvent", fileName = "New AudioEvent.asset")]
+public class AudioEvent : ScriptableObject
+{
+    [SerializeField] string eventName;
+    [SerializeField] AudioClip[] audioClips;
+
+    public AudioClip GetClip()
+    {
+        return audioClips[Random.Range(0, audioClips.Length - 1)];
+    }
+}
---
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioPlayer : MonoBehaviour
+{
+    [SerializeField] List<AudioEvent> audioEvents = new List<AudioEvent>();
+    [SerializeField] List<AudioSource> audioSources = new List<AudioSource>();
+    [SerializeField] int initialSources = 1;
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] AudioMixerGroup mixGroup;
+    [SerializeField] [Range(0, 1)] float spatialBlend = 0;
+    internal int playIndex = 0;
+
+    void Start()
+    {
+        if (audioSources.Count < initialSources)
+        {
+            for (int i = audioSources.Count; i < initialSources; ++i)
+            {
+                AddAudioSource();
+            }
+        }
+
+        foreach (AudioSource s in audioSources)
+        {
+            s.outputAudioMixerGroup = mixGroup;
+            s.spatialBlend = spatialBlend;
+        }
+    }
+
+    private void AddAudioSource()
+    {
+        var audioS = gameObject.AddComponent<AudioSource>();
+        audioSources.Add(audioS);
+    }
+
+    public void PlayAudioEvent(int eventIndex)
+    {
+        if (eventIndex < audioEvents.Count)
+        {
+            var clip = audioEvents[eventIndex].GetClip();
+            var source = GetNextSource();
+
+            source.clip = clip;
+            source.Play();
+        }
+        else
+        {
+            Debug.LogError("Event Index out of range.  You called index: " + eventIndex + " from: " + gameObject.name);
+        }
+    }
+
+    private AudioSource GetNextSource()
+    {
+        playIndex = (playIndex + 1) % audioSources.Count;
+
+        if (!audioSources[playIndex].isPlaying)
+        {
+            return audioSources[playIndex];
+        }
+        else
+        {
+            AddAudioSource();
+            GetNextSource();
+        }
+
+        return audioSources[playIndex];
+    }
+}

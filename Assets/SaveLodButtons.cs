bf0bcb88c21fc2039e9b7611ae05f4774651c1be
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2021-03-16 16:47:33 -0700

    improvements to editor save / load UI

--- Assets/SaveLodButtons.cs
+++ Assets/SaveLodButtons.cs
@@ -7,6 +7,11 @@ public class SaveLodButtons : MonoBehaviour
 {
     public GameObject SaveLoadUI;
 
+    public Button LoadButton;
+    public InputField inputField;
+
+    public Text SaveReminder;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,6 +38,34 @@ public class SaveLodButtons : MonoBehaviour
         SaveLoadUI.GetComponent<CanvasGroup>().alpha = 0;
         SaveLoadUI.GetComponent<CanvasGroup>().interactable = false;
         SaveLoadUI.GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void Save()
+    {
+        SaveReminder.gameObject.SetActive(true);
+
+        Vector2 temp = inputField.GetComponent<RectTransform>().position;
+
+        temp.y = 250;
+
+        inputField.GetComponent<RectTransform>().position = temp;
+
+        LoadButton.gameObject.SetActive(false);
+
+    }
+
+    public void LoadString()
+    {
+        SaveReminder.gameObject.SetActive(false);
+
+        Vector2 temp = inputField.GetComponent<RectTransform>().position;
+
+        temp.y = 265;
+
+        inputField.GetComponent<RectTransform>().position = temp;
+
+        inputField.text = "";
+        LoadButton.gameObject.SetActive(true);
     }
 
 }

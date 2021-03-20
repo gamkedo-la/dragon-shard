66cca3def61bcd025377f561e1c44389a91a9cca
rocleemusic <48lee@cardinalmail.cua.edu>
2021-03-20 11:45:59 -0400

    missed push

---
+++ Assets/Scripts/Audio/MusicManagerHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicManagerHelper : MonoBehaviour
+{
+    [SerializeField] AudioMixerSnapshot defaultSnapshot;
+    [SerializeField] AudioMixerSnapshot musicOffSnapshot;
+    [SerializeField] KeyCode defaultHotKey;
+    [SerializeField] KeyCode musicOffHotKey;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(defaultHotKey))
+            defaultSnapshot.TransitionTo(0.1f);
+
+        if (Input.GetKeyDown(musicOffHotKey))
+            musicOffSnapshot.TransitionTo(0.1f);
+    }
+}

20230899ea1ea3dc8e778871519ca27e2ff7e531
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2021-04-03 23:49:14 -0700

    Added pause menu

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public CanvasGroup PM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Open();
+        }
+
+    }
+    
+    public void Open()
+    {
+        PM.alpha = 1;
+        PM.blocksRaycasts = true;
+        PM.interactable = true;
+
+    }
+
+    public void Close()
+    {
+        PM.alpha = 0;
+        PM.blocksRaycasts = false;
+        PM.interactable = false;
+
+
+    }
+
+
+
+}
